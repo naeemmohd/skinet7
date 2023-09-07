440f0600a5c62c05942a715c1c2f5ae422fd32c4
Mohd Naeem <naeem.mohd@hotmail.com>
2023-09-06 23:59:56 -0500

    breadcrumbs, ngx-breadcrumb

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, OnSameUrlNavigation } from '@angular/router';
 import { Product } from 'src/app/shared/models/products';
 import { ShopService } from '../shop.service';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -13,7 +14,8 @@ export class ProductDetailsComponent implements OnInit {
 
   constructor(
     private shopService: ShopService,
-    private activatedRoute: ActivatedRoute // to get the active route 
+    private activatedRoute: ActivatedRoute, // to get the active route 
+    private bcService: BreadcrumbService // add Breadcrumb service
     ) {}
 
   ngOnInit(): void {
@@ -24,9 +26,11 @@ export class ProductDetailsComponent implements OnInit {
     const id = this.activatedRoute.snapshot.paramMap.get('id'); //gets the parameter
     //+ is a cast operator
     if (id) this.shopService.getProduct(+id).subscribe({
-      next: product => this.product = product,
+      next: product => {
+        this.product = product;
+        this.bcService.set('@productDetails', product.name); // set the alias value to product name
+      },
       error: error => console.log(error)
     })
   }
-
 }
