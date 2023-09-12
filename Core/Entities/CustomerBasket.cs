3ad1891afab692b4cca43ba83e3bb6dd8aa57051
Mohd Naeem <naeem.mohd@hotmail.com>
2023-09-11 23:57:32 -0500

    testing basket, setting up Redis with Docker

--- Core/Entities/CustomerBasket.cs
+++ Core/Entities/CustomerBasket.cs
@@ -2,15 +2,16 @@ namespace Core.Entities
 {
     public class CustomerBasket
     {
-        public string Id { get; set; }
-        public List<BasketItem> Items =new List<BasketItem>();
-    
         public CustomerBasket()
         {
         }
+
         public CustomerBasket(string id)
         {
             Id = id;
         }
+
+        public string Id { get; set; }
+        public List<BasketItem> Items { get; set; } = new List<BasketItem>();
     }
 }
