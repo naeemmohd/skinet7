1fe9cf4e5eb87e6d3a73f1e50e9b84b3c305b152
Mohd Naeem <naeem.mohd@hotmail.com>
2023-09-17 22:44:23 -0500

    Login functionality updates 02

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -14,8 +14,8 @@ export class LoginComponent {
 
   //create a new Reactive form
   loginForm = new FormGroup({
-    email: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required)
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
   });
 
   //on Submit
