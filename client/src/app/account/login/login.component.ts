86ef4b004d89352dba1f18f970a697014d6355dd
Mohd Naeem <naeem.mohd@hotmail.com>
2023-09-18 22:13:51 -0500

    Register functionality updates 01 using FormBuilder, handling ModelState errors

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -12,10 +12,13 @@ export class LoginComponent {
 
   constructor(private accountService: AccountService, private router: Router){}
 
+  regComplexPassword = "(?=^.{6,10}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$"
+  
+
   //create a new Reactive form
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required])
+    password: new FormControl('', [Validators.required, Validators.pattern(this.regComplexPassword)])
   });
 
   //on Submit
--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AccountService } from '../account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  //FormBuilder - reduces the amount of boilerplate needed to build complex forms
+  constructor(private formBuilder: FormBuilder, private accountService: AccountService, private router: Router) { }
+  regComplexPassword = "(?=^.{6,10}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$"
+  errors: string[] | null = null;
+  registerForm = this.formBuilder.group({
+    displayName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.pattern(this.regComplexPassword)]],
+  })  
 
+  onSubmit() {
+    this.accountService.register(this.registerForm.value).subscribe({
+      next: () => this.router.navigateByUrl('/shop'),
+      error: error => this.errors = error.errors
+    })
+  }
 }
